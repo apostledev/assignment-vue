1be877cabf1d01e411551d40dc1023db3e4f41ac
Bader Ali <>
2021-04-23 23:37:47 +0200

    Vite and TailwindCSS setup

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1 class="bg-green-500 text-white p-3 text-4xl text-center">Work In Progress</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App'
+})
+</script>
